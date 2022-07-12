353ae8c0adb5d242caddcd152eb7ad827f18489f
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-07-12 20:51:13 -0300

    Inclusão de texto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
 const Navbar = () => {
   return (
     <nav id="navbar">
       <h2>
-        <Link to="/">MoviesLib</Link>
+        <Link to="/">
+          <BiCameraMovie /> MoviesLib
+          </Link>
       </h2>
       <form>
         <input type="text" placeholder="Busque um filme" />
-        <button type="submit"></button>
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
       </form>
     </nav>
   );
