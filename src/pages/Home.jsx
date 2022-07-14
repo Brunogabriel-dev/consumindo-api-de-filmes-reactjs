cd1cd9c731498c3c24a8dc66eb1b6613a96ce4c4
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-07-13 22:53:01 -0300

    Inclusão de texto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,13 @@ const moviesURL = import.meta.env.VITE_API
 const apiKey = import.meta.env.VITE_API_KEY
 
 const Home = () => {
-  const [topMovies, setTopMovies] = useState([])
+  const [topMovies, setTopMovies] = useState([]);
 
   const getTopRatedMovies = async (url) => {
-    const res = await fetch(url)
-    const data = await res.json()
+    const res = await fetch(url);
+    const data = await res.json();
 
-    setTopMovies(data.results);
+    console.log(data);
   };
 
   useEffect(() => {
