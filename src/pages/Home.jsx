d57e7aa33ead8746f66f0d65c788fedd1d3aeec4
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-07-14 20:45:56 -0300

    Inclusão de texto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,9 @@ const Home = () => {
     <div className="container">
       <h2 className="title">Melhores filmes:</h2>
       <div className="movies-container">
-       {topMovies.length > 0 && topMovies.map((movie) => <p>{movie.title}</p>)}
+       {topMovies.length === 0 && <p>Carregando...</p>}
+       {topMovies.length > 0 && 
+       topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
       </div>
     </div>
   );
