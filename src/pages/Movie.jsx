56ffaf16d978707f4b2a4f52ab33dbbea8cace99
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-07-15 17:07:48 -0300

    Inclusão de texto

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -15,7 +15,30 @@ const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
 const Movie = () => {
-  return <div>Movie</div>
+  const {id} = useParams()
+  const [movie, setMovie] = useState(null)
+
+  const getMovie = async(url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setMovie(data);
+  };
+
+  useEffect(() => {
+    const movieUrl = `${moviesURL}${id}?${apiKey}`;
+    getMovie(movieUrl);
+  }, []);
+
+  return (
+  <div>
+    {movie && ( 
+    <>
+  <MovieCard movie={movie} showLink={false} />
+  </>
+  )}
+  </div>
+  );
 };
 
 export default Movie;
