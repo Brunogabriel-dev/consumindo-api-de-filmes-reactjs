3ea8edcae6dbd0671d2165259e060491d2c1ce7b
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-07-14 22:59:16 -0300

    Inclusão de texto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,12 @@ const Navbar = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(search);
-  }
+
+    if(!search) return
+
+    navigate(`/search?q=${search}`);
+    setSearch("");
+  };
 
   return (
     <nav id="navbar">
