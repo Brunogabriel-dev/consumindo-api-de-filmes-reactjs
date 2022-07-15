e1ec5468afa34feb17de50d15d38deaa44ae7f20
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-07-15 16:29:49 -0300

    Inclusão de texto

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,3 +1,14 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {
+  BsGraphUp,
+  BsWallet2,
+  BsHourglassSplit,
+  BsFillFileEarmarkTextFill
+} 
+
+
+
 const Movie = () => {
   return <div>Movie</div>
 };
