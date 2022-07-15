43f08fa35211b53148e4a4fc94d728c95b57baa3
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-07-15 16:41:45 -0300

    Inclusão de texto

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -5,9 +5,14 @@ import {
   BsWallet2,
   BsHourglassSplit,
   BsFillFileEarmarkTextFill
-} 
+} from 'react-icons/bs'
 
+import MovieCard from "../components/MovieCard";
 
+import './Movie.css'
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const Movie = () => {
   return <div>Movie</div>
