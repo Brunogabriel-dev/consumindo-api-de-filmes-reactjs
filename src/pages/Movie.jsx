070745b1eade3a6f8e562001b30ede030934885d
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-07-15 18:43:04 -0300

    Inclusão de texto

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -9,7 +9,7 @@ import {
 
 import MovieCard from "../components/MovieCard";
 
-import './Movie.css'
+import './Movie.css';
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -24,6 +24,13 @@ const Movie = () => {
 
     setMovie(data);
   };
+
+  const formatCurrency = (number) => {
+    return number.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    })
+  }
 
   useEffect(() => {
     const movieUrl = `${moviesURL}${id}?${apiKey}`;
@@ -40,13 +47,13 @@ const Movie = () => {
     <h3>
       <BsWallet2 /> Orçamento:
     </h3>
-    <p>{movie.budget}</p>
+    <p>{formatCurrency(movie.budget)}</p>
   </div>
   <div className="info">
     <h3>
       <BsGraphUp /> Receita:
     </h3>
-    <p>{movie.revenue}</p>
+    <p>{formatCurrency(movie.revenue)}</p>
   </div>
   <div className="info">
     <h3>
